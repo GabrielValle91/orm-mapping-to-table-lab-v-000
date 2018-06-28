7e60f0668d11252402c670362b042e542c7ddf7a
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 15:52:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,9 +13,7 @@ class Student
   end
 
   def self.create(name:, grade:)
-    student = Student.new
-    student.name = name
-    student.grade = grade
+    student = Student.new(name: name, grade: grade)
     student.save
     student
   end
